8cfe9000a5e784004447883c03f07d943498eed3
AltusDD <132203980+AltusDD@users.noreply.github.com>
2025-09-22 11:40:08 -0500

    initial commit of Azure Functions stubs and deployment workflow

---
+++ ping/__init__.py
@@ -0,0 +1,4 @@
+import json, azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse(json.dumps({"ok": True}), mimetype="application/json", status_code=200)
