8b21b940bfa0d184e0e8b467f063b0529f2fd63c
AltusDD <132203980+AltusDD@users.noreply.github.com>
2025-09-22 18:46:31 -0500

    Refactor response to return JSON formatted empty list

--- legal_cases/__init__.py
+++ legal_cases/__init__.py
@@ -1,4 +1,10 @@
+import json
 import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse("[]", mimetype="application/json", status_code=200)
+    # Minimal stub returning an empty list
+    return func.HttpResponse(
+        json.dumps([]),
+        mimetype="application/json",
+        status_code=200
+    )
