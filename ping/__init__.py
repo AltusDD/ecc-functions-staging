fbe2e8874a7324e2c26e08e24724395c5b7bcb26
AltusDD <132203980+AltusDD@users.noreply.github.com>
2025-09-22 18:45:07 -0500

    Format HttpResponse return statement for readability

--- ping/__init__.py
+++ ping/__init__.py
@@ -1,4 +1,9 @@
-import json, azure.functions as func
+import json
+import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse(json.dumps({"ok": True}), mimetype="application/json", status_code=200)
+    return func.HttpResponse(
+        json.dumps({"ok": True}),
+        mimetype="application/json",
+        status_code=200
+    )
